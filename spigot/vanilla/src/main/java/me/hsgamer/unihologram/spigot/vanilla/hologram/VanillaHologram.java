067fe36e7fd1bfa74278e3050623a3d5bc23e399
HSGamer <huynhqtienvtag@gmail.com>
2023-03-21 10:42:15 +0700

    run VanillaHologram in a sync task
    to support non-blocking sync update

--- spigot/vanilla/src/main/java/me/hsgamer/unihologram/spigot/vanilla/hologram/VanillaHologram.java
+++ spigot/vanilla/src/main/java/me/hsgamer/unihologram/spigot/vanilla/hologram/VanillaHologram.java
@@ -12,10 +12,13 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,8 +38,9 @@ public class VanillaHologram extends SimpleHologram<Location> implements Colored
     }
 
     private final List<Entity> entities = new ArrayList<>();
-    private final Object lock = new Object();
+    private final AtomicReference<List<HologramLine>> linesRef = new AtomicReference<>();
     private final Plugin plugin;
+    private BukkitTask task;
 
     /**
      * Create a new hologram
@@ -51,57 +55,56 @@ public class VanillaHologram extends SimpleHologram<Location> implements Colored
     }
 
     @Override
-    protected void updateHologram() {
-        Bukkit.getScheduler().runTask(plugin, () -> {
-            synchronized (lock) {
-                clearHologram();
-
-                World world = location.getWorld();
-                if (world == null) {
-                    return;
-                }
-
-                Location currentLocation = location.clone().add(0, -2, 0);
-                for (HologramLine line : lines) {
-                    currentLocation = currentLocation.clone().add(0, -0.27, 0);
-                    Entity entity;
-                    if (line instanceof ItemHologramLine && VERSION >= 10) {
-                        currentLocation = currentLocation.clone().add(0, -0.4, 0);
-                        Location itemLocation = currentLocation.clone().add(0, 2.2, 0);
-                        Item item = world.dropItem(itemLocation, ((ItemHologramLine) line).getContent());
-                        entity = item;
-                        entity.setGravity(false);
-                        entity.setVelocity(new Vector(0, 0, 0));
-                        entity.teleport(itemLocation);
-                        entity.setInvulnerable(true);
-                        item.setPickupDelay(Integer.MAX_VALUE);
-                        item.setCustomNameVisible(false);
-                    } else {
-                        ArmorStand armorStand = world.spawn(currentLocation, ArmorStand.class);
-                        entity = armorStand;
-                        armorStand.setGravity(false);
-                        armorStand.setVisible(false);
-                        armorStand.setCustomNameVisible(true);
-                        armorStand.setInvulnerable(true);
-                        armorStand.setCustomName(
-                                line instanceof TextHologramLine
-                                        ? colorize(((TextHologramLine) line).getContent())
-                                        : line.getRawContent()
-                        );
-                    }
-                    entities.add(entity);
-                }
-            }
-        });
-    }
-
-    @Override
     protected void initHologram() {
-        // EMPTY
+        task = Bukkit.getScheduler().runTaskTimer(plugin, this::updateHologramEntity, 5, 5);
     }
 
-    @Override
-    protected void clearHologram() {
+    private void updateHologramEntity() {
+        List<HologramLine> toUpdate = linesRef.getAndSet(null);
+        if (toUpdate == null) {
+            return;
+        }
+
+        clearHologram();
+
+        World world = location.getWorld();
+        if (world == null) {
+            return;
+        }
+
+        Location currentLocation = location.clone().add(0, -2, 0);
+        for (HologramLine line : toUpdate) {
+            currentLocation = currentLocation.clone().add(0, -0.27, 0);
+            Entity entity;
+            if (line instanceof ItemHologramLine && VERSION >= 10) {
+                currentLocation = currentLocation.clone().add(0, -0.4, 0);
+                Location itemLocation = currentLocation.clone().add(0, 2.2, 0);
+                Item item = world.dropItem(itemLocation, ((ItemHologramLine) line).getContent());
+                entity = item;
+                entity.setGravity(false);
+                entity.setVelocity(new Vector(0, 0, 0));
+                entity.teleport(itemLocation);
+                entity.setInvulnerable(true);
+                item.setPickupDelay(Integer.MAX_VALUE);
+                item.setCustomNameVisible(false);
+            } else {
+                ArmorStand armorStand = world.spawn(currentLocation, ArmorStand.class);
+                entity = armorStand;
+                armorStand.setGravity(false);
+                armorStand.setVisible(false);
+                armorStand.setCustomNameVisible(true);
+                armorStand.setInvulnerable(true);
+                armorStand.setCustomName(
+                        line instanceof TextHologramLine
+                                ? colorize(((TextHologramLine) line).getContent())
+                                : line.getRawContent()
+                );
+            }
+            entities.add(entity);
+        }
+    }
+
+    private void clearHologramEntity() {
         entities.forEach(entity -> {
             try {
                 entity.remove();
@@ -110,5 +113,19 @@ public class VanillaHologram extends SimpleHologram<Location> implements Colored
             }
         });
         entities.clear();
+    }
+
+    @Override
+    protected void updateHologram() {
+        linesRef.set(Collections.unmodifiableList(lines));
+    }
+
+    @Override
+    protected void clearHologram() {
+        if (task != null) {
+            task.cancel();
+            task = null;
+        }
+        Bukkit.getScheduler().runTask(plugin, this::clearHologramEntity);
     }
 }
