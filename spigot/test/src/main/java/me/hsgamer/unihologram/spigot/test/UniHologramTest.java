f46473c9346bb3c9f7c74a85f351451604a28355
HSGamer <huynhqtienvtag@gmail.com>
2023-04-03 11:09:00 +0700

    remove plugin-annotations

--- spigot/test/src/main/java/me/hsgamer/unihologram/spigot/test/UniHologramTest.java
+++ spigot/test/src/main/java/me/hsgamer/unihologram/spigot/test/UniHologramTest.java
@@ -2,13 +2,7 @@ package me.hsgamer.unihologram.spigot.test;
 
 import me.hsgamer.hscore.bukkit.baseplugin.BasePlugin;
 import me.hsgamer.unihologram.spigot.test.command.MainCommand;
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
-@Plugin(name = "UniHologramTest", version = "1.0.0")
-@ApiVersion(ApiVersion.Target.v1_13)
-@Dependency("UniHologram")
 public class UniHologramTest extends BasePlugin {
     private HologramManager hologramManager;
 
